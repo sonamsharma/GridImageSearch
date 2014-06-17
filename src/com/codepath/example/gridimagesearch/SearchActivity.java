a2ce55f7675424bf46f56ef9cb44471afe7a8695
Sonam Sharma <sonam.scu@gmail.com>
2014-06-17 12:38:20 -0700

    fourth commit - finished all the required user stories

--- src/com/codepath/example/gridimagesearch/SearchActivity.java
+++ src/com/codepath/example/gridimagesearch/SearchActivity.java
@@ -20,56 +20,67 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.GridView;
 import android.widget.Toast;
+
 import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.JsonHttpResponseHandler;
 
 public class SearchActivity extends Activity {
-EditText etQuery;
-Button btnSearch;
-GridView gvResults;
-ArrayList<ImageResult> imageResults = new ArrayList<ImageResult>();
-ImageResultArrayAdapter imageAdapter;
-AdvanceSettings settings;
-
+	EditText etQuery;
+	Button btnSearch;
+	GridView gvResults;
+	ArrayList<ImageResult> imageResults = new ArrayList<ImageResult>();
+	ImageResultArrayAdapter imageAdapter;
+	AdvanceSettings settings;
+	AsyncHttpClient client;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_search);
 		setupViews();
+
 		imageAdapter = new ImageResultArrayAdapter(this, imageResults);
 		gvResults.setAdapter(imageAdapter);
-		gvResults.setOnItemClickListener(new OnItemClickListener(){
-
+		gvResults.setOnItemClickListener(new OnItemClickListener() {
 			@Override
-			public void onItemClick(AdapterView<?> adapter, View parent, int position,
-					long rowId) {
+			public void onItemClick(AdapterView<?> adapter, View parent,
+					int position, long rowId) {
 				// TODO Auto-generated method stub
-				Intent i = new Intent(getApplicationContext(), ImageDisplayActivity.class);
+				Intent i = new Intent(getApplicationContext(),
+						ImageDisplayActivity.class);
 				ImageResult imageResult = imageResults.get(position);
 				i.putExtra("result", imageResult);
 				startActivity(i);
-				
 			}
-			
+		});
+
+		gvResults.setOnScrollListener(new EndlessScrollListener() {
+			@Override
+			public void onLoadMore(int page, int totalItemsCount) {
+				// Triggered only when new data needs to be appended to the list
+				// Add whatever code is needed to append new items to your
+				// AdapterView
+				// customLoadMoreDataFromApi(page);
+				customLoadMoreDataFromApi(totalItemsCount);
+			}
 		});
 	}
-	
-	public void onSettings(MenuItem mi){
-		Intent i = new Intent(this,AdvancedSearchActivity.class);
-		if(settings != null){
-			 i.putExtra("previous", settings);
+
+	public void onSettings(MenuItem mi) {
+		Intent i = new Intent(this, AdvancedSearchActivity.class);
+		if (settings != null) {
+			i.putExtra("previous", settings);
 		}
 		startActivityForResult(i, 123);
-		
-		
 	}
-	
+
 	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 		if (resultCode == RESULT_OK) {
 			if (requestCode == 123) {
-				settings = (AdvanceSettings) data.getSerializableExtra("filters");
-				Toast.makeText(this, settings.toString(), Toast.LENGTH_LONG).show();
+				settings = (AdvanceSettings) data
+						.getSerializableExtra("filters");
+				Toast.makeText(this, settings.toString(), Toast.LENGTH_LONG)
+						.show();
 			}
 		}
 	}
@@ -80,59 +91,72 @@ AdvanceSettings settings;
 		btnSearch = (Button) findViewById(R.id.btnSearch);
 		gvResults = (GridView) findViewById(R.id.gvResults);
 	}
-	
-	public void onImageSearch(View v){
+
+	public void onImageSearch(View v) {
+		imageResults.clear();
+		imageAdapter.clear();
+		customLoadMoreDataFromApi(0);
+	}
+
+	public void customLoadMoreDataFromApi(int page) {
 		String query = etQuery.getText().toString();
 		AsyncHttpClient client = new AsyncHttpClient();
-		String url = "https://ajax.googleapis.com/ajax/services/search/images?rsz=8&"+
-			"start=" +0+"&v=1.0&q="+ Uri.encode(query);
-		
-		String settingsFilter = new String();
-		if(settings != null){
-			settingsFilter = "&imgsz=";
-		
-			if(settings.getSize().equals("small")){
-				settingsFilter= settingsFilter+"icon";
-				
+		// Can use String.format
+		String url = "https://ajax.googleapis.com/ajax/services/search/images?rsz=8&"
+				+ "start=" + page + "&v=1.0&q=" + Uri.encode(query);
+
+		String settingsFilter = new String();// whats this? Use StringBuilder
+												// instead
+		if (settings != null) {
+			settingsFilter = "&imgsz=";// you can't change string... this way
+
+			if (settings.getSize().equals("small")) {
+				settingsFilter = settingsFilter + "icon";// this is wrong. You
+															// don't change
+															// string in java
 			}
-			if(settings.getSize().equals("medium")){
-				settingsFilter= settingsFilter+"small";
-				
+
+			if (settings.getSize().equals("medium")) {
+				settingsFilter = settingsFilter + "small";// same here user
+															// StringBuilder
+
 			}
-			else if(settings.getSize().equals("large")){
-				settingsFilter= settingsFilter+"xxlarge";
-				
+
+			else if (settings.getSize().equals("large")) {
+				settingsFilter = settingsFilter + "xxlarge";
+
+			} else if (settings.getSize().equals("x-large")) {
+				settingsFilter = settingsFilter + "huge";
+
+			} else {
+				settingsFilter = settingsFilter + "small";
 			}
-			else if(settings.getSize().equals("x-large")){
-				settingsFilter= settingsFilter+"huge";
-				
-			}
-			else{
-					settingsFilter=settingsFilter+"small";
-			}
-			settingsFilter = settingsFilter + "&imgtype"+settings.getType()+ "&imgcolor="+
-							 settings.getColor() + "&as_sitesearch="+
-							 settings.getSiteFilter();
-							 
-			
+			settingsFilter = settingsFilter + "&imgtype" + settings.getType()
+					+ "&imgcolor=" + settings.getColor() + "&as_sitesearch="
+					+ settings.getSiteFilter();
+
 		}
-		
-		Toast.makeText(this, "Fetching images for : " + url + settingsFilter, Toast.LENGTH_LONG).show();
-;		client.get(url+settingsFilter, new JsonHttpResponseHandler(){
-		@Override
-		public void onSuccess(JSONObject response){
-			JSONArray imageJsonResults = null;
-			try{
-				imageJsonResults = response.getJSONObject("responseData").getJSONArray("results");
-				imageResults.clear();
-				imageAdapter.addAll(ImageResult.fromJSONArray(imageJsonResults));
-				Log.d("DEBUG",imageResults.toString());
-			}catch(JSONException e){
-				e.printStackTrace();
+
+		// Toast.makeText(this, "Fetching images for : " + url + settingsFilter,
+		// Toast.LENGTH_LONG).show();
+
+		client.get(url + settingsFilter, new JsonHttpResponseHandler() {
+			@Override
+			public void onSuccess(JSONObject response) {
+				JSONArray imageJsonResults = null;
+				try {
+					imageJsonResults = response.getJSONObject("responseData")
+							.getJSONArray("results");
+					// imageResults.clear();
+					imageAdapter.addAll(ImageResult
+							.fromJSONArray(imageJsonResults));
+					Log.d("DEBUG", imageResults.toString());
+				} catch (JSONException e) {
+					e.printStackTrace();
+				}
+
 			}
-			
-		}
-});
+		});
 	}
 
 	@Override
@@ -141,5 +165,4 @@ AdvanceSettings settings;
 		getMenuInflater().inflate(R.menu.search, menu);
 		return true;
 	}
-
 }
